b4df19bc0a0b9b3b3c40ddbbd8d7a2b1d59e5262
Nasty <goldenpear@ya.ru>
2020-04-06 12:59:53 +0300

    homework 01 gradle

---
+++ hw01-gradle/src/main/java/otus/java/HelloOtus.java
@@ -0,0 +1,17 @@
+package otus.java;
+
+import com.google.common.base.Charsets;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+
+public class HelloOtus {
+
+    public static void main(String[] args) {
+        HashFunction hash = Hashing.sha256();
+        HashCode hashCode = hash.newHasher()
+                .putString(HelloOtus.class.getName(), Charsets.UTF_8)
+                .hash();
+        System.out.println(hashCode);
+    }
+}
