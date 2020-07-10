1c457a6ee2ee81b9ca3f78a44cf2d5616ef0340a
Nasty <goldenpear@ya.ru>
2020-07-10 16:26:40 +0300

    hw13-di реализованы методы класса AppComponentsContainerImpl

--- hw13-di/src/main/java/ru/otus/java/appcontainer/AppComponentsContainerImpl.java
+++ hw13-di/src/main/java/ru/otus/java/appcontainer/AppComponentsContainerImpl.java
@@ -1,12 +1,19 @@
 package ru.otus.java.appcontainer;
 
+import lombok.extern.slf4j.Slf4j;
+import ru.otus.java.appcontainer.api.AppComponent;
 import ru.otus.java.appcontainer.api.AppComponentsContainer;
 import ru.otus.java.appcontainer.api.AppComponentsContainerConfig;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.*;
 
+@Slf4j
 public class AppComponentsContainerImpl implements AppComponentsContainer {
-
+    private Class<?> configClass;
+    private Object configObject;
     private final List<Object> appComponents = new ArrayList<>();
     private final Map<String, Object> appComponentsByName = new HashMap<>();
 
@@ -16,7 +23,83 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private void processConfig(Class<?> configClass) {
         checkConfigClass(configClass);
-        // You code here...
+        this.configClass = configClass;
+        try {
+            this.configObject = initConfigObject();
+            findComponentMethodsInConfigClass();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            log.debug(e.getLocalizedMessage());
+        }
+    }
+
+    private void findComponentMethodsInConfigClass() {
+        Method[] methodsPublic = configClass.getDeclaredMethods();
+        List<Method> methodsAppComponent = new ArrayList<>();
+        Arrays.stream(methodsPublic).forEach(method -> {
+            try {
+                if (configClass.getMethod(method.getName(), method.getParameterTypes()).isAnnotationPresent(AppComponent.class)) {
+                    methodsAppComponent.add(method);
+                }
+            } catch (NoSuchMethodException e) {
+                log.debug(e.getLocalizedMessage());
+            }
+        });
+
+        saveComponents(methodsAppComponent);
+    }
+
+    private void saveComponents(List<Method> methodsAppComponent) {
+        methodsAppComponent.stream()
+                .sorted(Comparator.comparing(method -> method.getAnnotation(AppComponent.class).order()))
+                .forEach(method -> {
+                    Parameter[] methodParameters = method.getParameters();
+                    if (methodParameters.length == 0) {
+                        initComponentObject(method);
+                    } else {
+                        Object[] arg = getObjectToParameters(methodParameters);
+                        initComponentObject(method, arg);
+                    }
+                });
+    }
+
+    private Object[] getObjectToParameters(Parameter[] methodParameters) {
+        List<Object> arg = new ArrayList<>();
+        for (Parameter param : methodParameters) {
+            Class<?> paramClazz = param.getType();
+            Optional<Object> objectInComponent = getObjectFromComponent(paramClazz);
+            objectInComponent.ifPresent(arg::add);
+        }
+        return arg.toArray();
+    }
+
+    private Optional<Object> getObjectFromComponent(Class<?> clazz) {
+        return appComponents.stream()
+                .filter(obj -> obj.getClass().equals(clazz)
+                        || (clazz.isInterface()
+                        && List.of(obj.getClass().getInterfaces()).contains(clazz))).findFirst();
+    }
+
+    private void initComponentObject(Method method) {
+        try {
+            Object objComponent = method.invoke(configObject);
+            putObjectToComponents(method, objComponent);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            log.debug(e.getLocalizedMessage());
+        }
+    }
+
+    private void initComponentObject(Method method, Object... arg) {
+        try {
+            Object objComponent = method.invoke(configObject, arg);
+            putObjectToComponents(method, objComponent);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            log.debug(e.getLocalizedMessage());
+        }
+    }
+
+    private void putObjectToComponents(Method method, Object objComponent) {
+        appComponents.add(objComponent);
+        appComponentsByName.put(method.getAnnotation(AppComponent.class).name(), objComponent);
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -26,12 +109,20 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+        Optional<Object> component = getObjectFromComponent(componentClass);
+        return (C) component.orElse(null);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public <C> C getAppComponent(String componentName) {
-        return null;
+        return (C) appComponentsByName.getOrDefault(componentName, null);
+    }
+
+    private Object initConfigObject() throws InstantiationException, IllegalAccessException, InvocationTargetException,
+            NoSuchMethodException {
+        return configClass.getDeclaredConstructor().newInstance();
     }
 }
